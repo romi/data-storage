0cc41aa83f7aa611043ef264e23c91d48eeea9cd
Jonathan Legrand <jonathan.legrand@ens-lyon.fr>
2020-09-10 19:29:38 +0200

    Add examples & options to Docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+###############################################################################
+# Example usages:
+###############################################################################
+# 1. Default build options will create `roboticsmicrofarms/romidb:latest`:
+# $ ./build.sh
+#
+# 2. Build image for a 'scanner' user and specify user & group id value:
+# $ ./build.sh -u scanner --uid 1003 --gid 1003
+#
+# 3. Build image with 'debug' image tag & another 'romidata' branch options:
+# $ ./build.sh -t debug -b 'feature/docker'
+
 user=$USER
+uid=$(id -u)
+gid=$(id -g)
 vtag="latest"
 git_branch='dev'
 
@@ -19,6 +33,12 @@ usage() {
     "
   echo "  -u, --user
     User name to create inside docker image, default to '$user'.
+    "
+  echo "  --uid
+    User id to use with 'user' inside docker image, default to '$uid'.
+    "
+  echo "  --gid
+    Group id to use with 'user' inside docker image, default to '$gid'.
     "
   echo "  -b, --branch
     Git branch to use for cloning 'romidata' inside docker image, default to '$git_branch'.
@@ -56,5 +76,7 @@ done
 
 docker build -t roboticsmicrofarms/romidb:$vtag \
   --build-arg USER_NAME=$user \
+  --build-arg USER_ID=$uid \
+  --build-arg GROUP_ID=$gid \
   --build-arg ROMIDATA_BRANCH=$git_branch \
   .
